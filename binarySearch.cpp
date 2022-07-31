035dbadf3188defb24fdf8271bf7bfbe263f58ee
Rishi3938 <97890700+Rishi3938@users.noreply.github.com>
2022-07-31 23:31:36 +0530

    Add files via upload

---
+++ binarySearch.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+ int binarySearch( int arr[], int n ,int key) 
+ {
+     int start = 0;
+     int end  = n-1;
+     int mid  = (start+end)/2;
+     
+     while(start<=end)
+     {
+         if(arr[mid] == key)
+         {
+             return mid;
+         }
+         
+         if(key>arr[mid])
+         {
+             start = mid+1;
+         }
+         else
+         {
+             end = mid-1;
+         }
+         
+         mid = (start+end)/2;
+     }
+return -1;     
+ }
+
+
+
+int main() {
+
+int even [6] = {2,4,5,8,10,12};
+
+int evenIndex = binarySearch(even,6,10);
+cout<<"index is "<<evenIndex<<" ";
+	return 0;
+}
