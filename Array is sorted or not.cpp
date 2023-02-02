2479039c8b094980c55cb4b17cfcdd0eaeeff4c1
Rishi3938 <97890700+Rishi3938@users.noreply.github.com>
2023-02-02 20:18:22 +0530

    Add files via upload

---
+++ Array is sorted or not.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool arrayISsorted( int arr[],int n){
+    
+    for(int i =1;i<n;i++){
+        if(arr[i]<arr[i-1])
+        return false;
+    }
+    return true;
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i =0;i<n;i++){
+        cin>>arr[i];
+    }
+    
+    if(arrayISsorted(arr,n)){
+        cout<<" true";
+    }
+    else{
+        cout<<"false";
+    }
+}
